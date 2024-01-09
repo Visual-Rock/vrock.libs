408c3e43a0c3143ded382a44c5e4deeabbbbe3a8
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2024-01-09 21:17:25 +0100

    log: WIP

--- log/src/include/vrock/log/Sinks/FileSinks.hpp
+++ log/src/include/vrock/log/Sinks/FileSinks.hpp
@@ -2,21 +2,65 @@
 
 #include "Sink.hpp"
 
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 
 namespace vrock::log
 {
+    /**
+     * @brief The FileSink class represents a log sink that writes log messages to a file.
+     *
+     * This class is derived from the Sink base class and overrides the log and flush functions to handle log messages
+     * by writing them to a specified file. The log messages are formatted based on the provided pattern.
+     */
     class FileSink final : public Sink
     {
     public:
+        /**
+         * @brief Constructor for the FileSink class.
+         *
+         * @param path The path to the file where log messages will be written.
+         * @param pattern A string_view representing the custom log message pattern.
+         *                Defaults to the global pattern if not provided.
+         */
         explicit FileSink( const std::filesystem::path &path, std::string_view pattern = get_global_pattern( ) );
+
+        /**
+         * @brief Destructor for the FileSink class.
+         */
         ~FileSink( ) override;
 
+        /**
+         * @brief Handles log messages by writing them to the specified file.
+         *
+         * @param message The log message to be processed.
+         */
         void log( const Message &message ) override;
+
+        /**
+         * @brief Flushes any buffered content to the file.
+         */
         void flush( ) override;
 
     private:
+        std::ofstream file_; ///< The ofstream object for writing log messages to the file.
+    };
+
+    class DailyFileSink final : public Sink
+    {
+    public:
+        DailyFileSink( std::string_view path, std::chrono::hours hour, std::chrono::minutes minutes,
+                       std::uint16_t max_files = 0, bool truncate = false, std::string_view = get_global_pattern( ) );
+
+    private:
+        std::string_view filename_format;
+        formatter_collection_t filename_formatters_;
+        std::chrono::time_point<std::chrono::system_clock> time;
         std::ofstream file_;
+        std::chrono::hours hour_;
+        std::chrono::minutes minutes_;
+        std::uint16_t max_files_;
+        bool truncate_;
     };
 } // namespace vrock::log
--- log/src/src/Sinks/FileSinks.cpp
+++ log/src/src/Sinks/FileSinks.cpp
@@ -21,4 +21,20 @@ namespace vrock::log
     {
         file_ << std::flush;
     }
+
+    auto check_file_time( std::chrono::time_point<std::chrono::system_clock> time, std::chrono::hours hours,
+                          std::chrono::minutes minutes ) -> bool
+    {
+    }
+
+    DailyFileSink::DailyFileSink( std::string_view path, std::chrono::hours hour, std::chrono::minutes minutes,
+                                  std::uint16_t max_files, bool truncate, std::string_view pattern )
+        : Sink( pattern, false ), hour_( hour ), minutes_( minutes ), max_files_( max_files ), truncate_( truncate )
+    {
+        if ( hour.count( ) > 23 || minutes.count( ) > 59 )
+            throw std::runtime_error( "rotation time has to be smaller than 23 hours and 59 minutes" );
+        const auto tp = std::chrono::system_clock::now( );
+        time = tp - std::chrono::days( 1 ) + std::chrono::hours( hour_ ) + std::chrono::minutes( minutes );
+    }
+
 } // namespace vrock::log
